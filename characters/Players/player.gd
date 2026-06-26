4628c33b51b466790f0bd235b1dd9b977553c403
moataz1411 <mezoahmed1411@gmail.com>
2026-06-26 04:20:53 +0300

    Add level, player movement, and tileset collisions
    Add level_1 scene with a TileMapLayer, player instance, Camera2D and Sprite using a new art/Image.png asset. Implement player movement script with horizontal movement and double-jump mechanics, and update the player scene to use the run animation.

--- characters/Players/player.gd
+++ characters/Players/player.gd
@@ -1,1 +1,27 @@
 extends CharacterBody2D
+
+const SPEED = 120.0
+const JUMP_VELOCITY = -250.0
+
+const MAX_JUMPS=2
+var jumps_left=MAX_JUMPS
+
+func _physics_process(delta: float) -> void:
+
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		jumps_left=MAX_JUMPS
+
+	if Input.is_action_just_pressed("up") and jumps_left > 0:
+		velocity.y = JUMP_VELOCITY
+		jumps_left -= 1
+
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
