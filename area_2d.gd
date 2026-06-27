232fdd52a06d7e9729bedf381ece47239bb81148
moataz1411 <mezoahmed1411@gmail.com>
2026-06-27 12:49:51 +0300

    Add Level 5, main menu, and level labels
    Added Level 5 complete with platforms, coins, and enemies. Created main menu scene with start/options/quit buttons.

--- area_2d.gd
+++ area_2d.gd
@@ -4,7 +4,7 @@ func _on_body_entered(body):
 	if body is CharacterBody2D:
 		Global.scene += 1
 
-		if Global.scene > 4:
+		if Global.scene > 5:
 			Global.scene = 1
 
 		get_tree().change_scene_to_file("res://Levels/level_" + str(Global.scene) + ".tscn")
