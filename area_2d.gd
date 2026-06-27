f349ea5c0f3f6164b9a2f6eb52854a53c514c857
moataz1411 <mezoahmed1411@gmail.com>
2026-06-27 04:55:41 +0300

    Add Level 4 and update level progression system
    Add a new Level 4 scene with platforms, coins, and enemies. Update Level 2 and 3 with repositioned UI elements and new Area2D instances for level progression.

--- area_2d.gd
+++ area_2d.gd
@@ -1,17 +1,10 @@
 extends Area2D
 
+func _on_body_entered(body):
+	if body is CharacterBody2D:
+		Global.scene += 1
 
-func _ready() -> void:
-	pass # Replace with function body.
+		if Global.scene > 4:
+			Global.scene = 1
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_body_entered(body: Node2D) -> void:
-	
-	Global.scene+=1
-	get_tree().change_scene_to_file("res://Levels/level_" + str(Global.scene) + ".tscn")
-	
+		get_tree().change_scene_to_file("res://Levels/level_" + str(Global.scene) + ".tscn")
